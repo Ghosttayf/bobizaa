4d1023379a752eb35c295893eb48703fe7b6d0f0
Ghosttayf <basharessamrayash@gmail.com>
2024-05-27 07:36:52 +0300

    Update join.js

--- plugins/join.js
+++ plugins/join.js
@@ -1,21 +1,20 @@
-let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})( [0-9]{1,3})?/i
+let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i;
 
-let handler = async (m, { conn, text, isOwner }) => {
-    let [_, code, expired] = text.match(linkRegex) || []
-    if (!code) throw 'الرابط غير صالح'
-    let res = await conn.groupAcceptInvite(code)
-    expired = Math.floor(Math.min(999, Math.max(1, isOwner ? isNumber(expired) ? parseInt(expired) : 0 : 3)))
-    m.reply(`تم الانضمام للمجموعة بنجاح ${res}${expired ? ` خلال ${expired} يوم` : ''}\n\nتابع صاحب البوت في قناته\https://whatsapp.com/channel/0029VaFpLsm2ZjCtcStqqd0h`)
-    let chats = global.db.data.chats[res]
-    if (!chats) chats = global.db.data.chats[res] = {}
-    if (expired) chats.expired = +new Date() + expired * 1000 * 60 * 60 * 24
-}
-handler.help = ['join']
-handler.tags = ['owner']
-
-handler.command = /^join$/i
-handler.rowner = true
-
-export default handler
-
-const isNumber = (x) => (x = parseInt(x), typeof x === 'number' && !isNaN(x))
+let handler = async (m, { conn, text }) => {
+  let [_, code] = text.match(linkRegex) || [];
+  if (!code) throw "*مثال*\n*.join ( https://chat.whatsapp.com/EdO9LD8OMZBFUXlqX68naV )*";
+  try {
+    const res = await conn.groupAcceptInvite(code);
+  } catch (e) {
+    throw `Error`;
+  } finally {
+    m.reply(
+      `تم الانضمام بنجاح لـــ  : *[ ${(await conn.groupGetInviteInfo(code)).id} ]*`,
+    );
+  }
+};
+handler.help = ["join"];
+handler.tags = ["owner"];
+handler.command = ["join"]
+handler.owner = true
+export default handler ;
